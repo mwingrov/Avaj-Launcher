978e2bdd74e74d7a89764a639c81cd3532022121
Mark WINGROVE <mwingrov@c1r4s1.wethinkcode.co.za>
2018-11-25 23:23:50 +0200

    new changes

--- factory/AircraftFactory.java
+++ factory/AircraftFactory.java
@@ -1,9 +1,30 @@
 package factory;
 
+import aircraft.Baloon;
+import aircraft.Coordinates;
+import aircraft.Helicopter;
+import aircraft.JetPlane;
 import flyable.Flyable;
 
 public class AircraftFactory {
-    public Flyable newAircraft(String type, int longitude, int latitide, int height){
-        if (type.equals())
+    public static Flyable newAircraft(String type, String name, int longitude, int latitude, int height){
+        Coordinates coordinates = new Coordinates(longitude, latitude, height);
+        Helicopter helicopter;
+        Baloon baloon;
+        JetPlane jetPlane;
+        if (type.equals("helicopter")){
+            helicopter = new Helicopter(name, coordinates);
+            return helicopter;
+        }
+        else if (type.equals("baloon")){
+            baloon = new Baloon(name, coordinates);
+            return baloon;
+        }
+        else if (type.equals("JetPlane")){
+            jetPlane = new JetPlane(name, coordinates);
+            return jetPlane;
+        }
+        else
+            return null;
     }
 }
